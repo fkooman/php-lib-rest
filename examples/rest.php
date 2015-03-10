30c1abc9c54602a8aaa9da12055f4812fecce501
François Kooman <fkooman@tuxed.net>
2015-03-10 11:00:29 +0100

    remove redundant use Exception

--- examples/rest.php
+++ examples/rest.php
@@ -21,7 +21,6 @@ require_once dirname(__DIR__).'/vendor/autoload.php';
 use fkooman\Http\JsonResponse;
 use fkooman\Rest\Service;
 use fkooman\Http\Exception\BadRequestException;
-use Exception;
 
 try {
     $service = new Service();
