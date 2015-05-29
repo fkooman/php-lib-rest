64c714d5c03df2d56ad63b6896438f6c5e03c1c7
François Kooman <fkooman@tuxed.net>
2015-05-29 15:34:40 +0200

    finish code coverage for PluginRegistry

--- tests/fkooman/Rest/PluginRegistryTest.php
+++ tests/fkooman/Rest/PluginRegistryTest.php
@@ -18,25 +18,25 @@
 
 namespace fkooman\Rest;
 
-use fkooman\Rest\Plugin\ReferrerCheckPlugin;
+use StdClass;
 use fkooman\Http\Request;
+use fkooman\Http\Response;
 use PHPUnit_Framework_TestCase;
 
 class PluginRegistryTest extends PHPUnit_Framework_TestCase
 {
-    /**
-     * @expectedException fkooman\Http\Exception\BadRequestException
-     * @expectedExceptionMessage HTTP_REFERER header missing
-     */
-    public function testDefaultPluginNotDisabled()
+    public function testDefaultPluginNotDisabledReturnResponse()
     {
+        $stub = $this->getMockBuilder('fkooman\Rest\ServicePluginInterface')->setMockClassName('Stub')->getMock();
+        $stub->method('execute')->willReturn(new Response());
+        
         $srv = array(
             'SERVER_NAME' => 'www.example.org',
             'SERVER_PORT' => 80,
             'QUERY_STRING' => '',
             'REQUEST_URI' => '/',
             'SCRIPT_NAME' => '/index.php',
-            'REQUEST_METHOD' => 'POST',
+            'REQUEST_METHOD' => 'GET',
         );
         $request = new Request($srv);
         $route = new Route(
@@ -46,44 +46,23 @@ class PluginRegistryTest extends PHPUnit_Framework_TestCase
             }
         );
         $p = new PluginRegistry();
-        $p->registerDefaultPlugin(new ReferrerCheckPlugin());
-        $p->run($request, $route);
+        $p->registerDefaultPlugin($stub);
+        $response = $p->run($request, $route);
+        $this->assertInstanceOf('fkooman\Http\Response', $response);
     }
 
-    public function testDefaultPluginDisabled()
+    public function testDefaultPluginNotDisabledReturnObject()
     {
+        $stub = $this->getMockBuilder('fkooman\Rest\ServicePluginInterface')->setMockClassName('Stub')->getMock();
+        $stub->method('execute')->willReturn((object) array('foo' => 'bar'));
+        
         $srv = array(
             'SERVER_NAME' => 'www.example.org',
             'SERVER_PORT' => 80,
             'QUERY_STRING' => '',
             'REQUEST_URI' => '/',
             'SCRIPT_NAME' => '/index.php',
-            'REQUEST_METHOD' => 'POST',
-        );
-        $request = new Request($srv);
-        $route = new Route(
-            array('GET'),
-            '/',
-            function () {
-            },
-            array(
-                'fkooman\Rest\Plugin\ReferrerCheckPlugin' => array('enabled' => false)
-            )
-        );
-        $p = new PluginRegistry();
-        $p->registerDefaultPlugin(new ReferrerCheckPlugin());
-        $p->run($request, $route);
-    }
-
-    public function testOptionalPluginNotEnabled()
-    {
-        $srv = array(
-            'SERVER_NAME' => 'www.example.org',
-            'SERVER_PORT' => 80,
-            'QUERY_STRING' => '',
-            'REQUEST_URI' => '/',
-            'SCRIPT_NAME' => '/index.php',
-            'REQUEST_METHOD' => 'POST',
+            'REQUEST_METHOD' => 'GET',
         );
         $request = new Request($srv);
         $route = new Route(
@@ -93,23 +72,23 @@ class PluginRegistryTest extends PHPUnit_Framework_TestCase
             }
         );
         $p = new PluginRegistry();
-        $p->registerOptionalPlugin(new ReferrerCheckPlugin());
-        $p->run($request, $route);
+        $p->registerDefaultPlugin($stub);
+        $response = $p->run($request, $route);
+        $this->assertObjectHasAttribute('foo', $response['stdClass']);
     }
 
-    /**
-     * @expectedException fkooman\Http\Exception\BadRequestException
-     * @expectedExceptionMessage HTTP_REFERER header missing
-     */
-    public function testOptionalPluginEnabled()
+    public function testDefaultPluginDisabledReturnObject()
     {
+        $stub = $this->getMockBuilder('fkooman\Rest\ServicePluginInterface')->setMockClassName('Stub')->getMock();
+        $stub->method('execute')->willReturn((object) array('foo' => 'bar'));
+        
         $srv = array(
             'SERVER_NAME' => 'www.example.org',
             'SERVER_PORT' => 80,
             'QUERY_STRING' => '',
             'REQUEST_URI' => '/',
             'SCRIPT_NAME' => '/index.php',
-            'REQUEST_METHOD' => 'POST',
+            'REQUEST_METHOD' => 'GET',
         );
         $request = new Request($srv);
         $route = new Route(
@@ -117,12 +96,64 @@ class PluginRegistryTest extends PHPUnit_Framework_TestCase
             '/',
             function () {
             },
-            array(
-                'fkooman\Rest\Plugin\ReferrerCheckPlugin' => array('enabled' => true)
-            )
+            array('Stub' => array('enabled' => false))
         );
         $p = new PluginRegistry();
-        $p->registerOptionalPlugin(new ReferrerCheckPlugin());
-        $p->run($request, $route);
+        $p->registerDefaultPlugin($stub);
+        $response = $p->run($request, $route);
+        $this->assertEquals(array(), $response);
+    }
+
+    public function testOptionalPluginNotEnabledReturnObject()
+    {
+        $stub = $this->getMockBuilder('fkooman\Rest\ServicePluginInterface')->setMockClassName('Stub')->getMock();
+        $stub->method('execute')->willReturn((object) array('foo' => 'bar'));
+        
+        $srv = array(
+            'SERVER_NAME' => 'www.example.org',
+            'SERVER_PORT' => 80,
+            'QUERY_STRING' => '',
+            'REQUEST_URI' => '/',
+            'SCRIPT_NAME' => '/index.php',
+            'REQUEST_METHOD' => 'GET',
+        );
+        $request = new Request($srv);
+        $route = new Route(
+            array('GET'),
+            '/',
+            function () {
+            }
+        );
+        $p = new PluginRegistry();
+        $p->registerOptionalPlugin($stub);
+        $response = $p->run($request, $route);
+        $this->assertEquals(array(), $response);
+    }
+
+    public function testOptionalPluginEnabledReturnObject()
+    {
+        $stub = $this->getMockBuilder('fkooman\Rest\ServicePluginInterface')->setMockClassName('Stub')->getMock();
+        $stub->method('execute')->willReturn((object) array('foo' => 'bar'));
+        
+        $srv = array(
+            'SERVER_NAME' => 'www.example.org',
+            'SERVER_PORT' => 80,
+            'QUERY_STRING' => '',
+            'REQUEST_URI' => '/',
+            'SCRIPT_NAME' => '/index.php',
+            'REQUEST_METHOD' => 'GET',
+        );
+        $request = new Request($srv);
+        $route = new Route(
+            array('GET'),
+            '/',
+            function () {
+            },
+            array('Stub' => array('enabled' => true))
+        );
+        $p = new PluginRegistry();
+        $p->registerOptionalPlugin($stub);
+        $response = $p->run($request, $route);
+        $this->assertObjectHasAttribute('foo', $response['stdClass']);
     }
 }
